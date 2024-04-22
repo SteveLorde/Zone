8bc627bc61cda30feb0c92005f33c6622246d17e
SteveLorde <mostafa.maher98@gmail.com>
2024-04-23 00:12:06 +0200

    practicing navbar dropdowd menus, added 2 floating animations in Animations.module.scss

--- src/Views/Components/Navbar/Navbar.tsx
+++ src/Views/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import "./NavbarStyle.module.scss";
 import {useContext} from "react";
-import {MainContext} from "../../Services/State/MainContext.tsx";
+import {MainContext} from "../../../Services/State/MainContext.tsx";
 
 export function Navbar() {
     const {setSelectedTab} = useContext(MainContext);
@@ -21,13 +21,33 @@ export function Navbar() {
     }
 
     return <>
-
-
         <div className="grid grid-cols-3 gap-4 place-items-center bg-blue-500">
             <img className="" onClick={() => ToggleMenu()} id="MenuButton" src="" alt="navmenu"/>
             <div id="NavMenu" className="navbarmenu bg-blue-500">
-                <button onClick={() => SelectTab(0)}>Profile</button>
-                <button onClick={() => SelectTab(1)}>Zone</button>
+                <ul>
+                    <li>
+                        <button id="ProfileBtn" className={"navbarbtn navbarbtn1"} onClick={() => SelectTab(0)}>Profile</button>
+                        <ul className={"dropdownlist dropdown1"}>
+                            <li>
+                                <button onClick={() => SelectTab(0)}>Dropdown button</button>
+                            </li>
+                            <li>
+                                <button onClick={() => SelectTab(0)}>Dropdown button</button>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <button id="ZoneBtn" className={"navbarbtn navbarbtn2"} onClick={() => SelectTab(1)}>Zone</button>
+                        <ul className={"dropdownlist dropdown2"}>
+                            <li>
+                                <button onClick={() => SelectTab(0)}>Dropdown button</button>
+                            </li>
+                            <li>
+                                <button onClick={() => SelectTab(0)}>Dropdown button</button>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
     </>;
